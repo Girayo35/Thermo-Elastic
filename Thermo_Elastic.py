e29b9508e99f27ee9ce2ff0a7235b2787828326e
Girayo35 <giray__ozen@hotmail.com>
2025-07-22 07:56:02 +0000

    Working Copy

--- Thermo_Elastic.py
+++ Thermo_Elastic.py
@@ -6,7 +6,7 @@ import random
 
 wb=openpyxl.load_workbook("Thermo_Elastic_Data.xlsx")
 ws=wb.active
-
+print ("here")
 file_name=["_ASG1_GLOBAL_MODEL_assyfem1_sim1-DSEOL_nastran_","_ASG1_GLOBAL_MODEL_assyfem1_sim1-JSEOL_nastran_","_ASG1_GLOBAL_MODEL_assyfem1_sim1-SMEQBOL_nastran_"]
 time_steps=["t129120","t133200","t137760","t146400","t155040","t163680","t170160","t176400","t180960","t189600","t198240","t206880","t215520"]
 i=0
